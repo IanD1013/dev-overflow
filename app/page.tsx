9033c8820b786b4d251b20ba8675e4810b48819d
IanD1013 <ian.tung1013@gmail.com>
2024-10-30 10:46:22 +1300

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-violet-700">
+    <h1 className="text-3xl font-black text-light-500">
       Welcome to the world of Next.js
     </h1>
   );
