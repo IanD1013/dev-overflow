40c0c48cd95cf4fe0fbfd9817aac851299628e98
IanD1013 <ian.tung1013@gmail.com>
2024-11-05 20:24:53 +1300

    Implement GitHub sign in

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -4,13 +4,18 @@ import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import { toast } from "@/hooks/use-toast";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constants/routes";
 
 const SocialAuthForm = () => {
   const buttonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
 
   const handleSignIn = async (provider: "github" | "google") => {
     try {
-      throw new Error("Not implemented yet");
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
     } catch (error) {
       console.log(error);
 
