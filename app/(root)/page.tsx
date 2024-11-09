b2609044deed23c8bcf57fb66098aae7cbccbfce
IanD1013 <ian.tung1013@gmail.com>
2024-11-10 10:58:54 +1300

    Implement home filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,33 @@
+import Link from "next/link";
+
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import Link from "next/link";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
   return (
     <>
-      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
         <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900" asChild>
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
+
       <section className="mt-11">
         <LocalSearch route="/" imgSrc="/icons/search.svg" placeholder="Search questions..." otherClasses="flex-1" />
       </section>
-      HomeFilter
+
+      <HomeFilter />
+
       <div className="mt-10 flex w-full flex-col gap-6">
         {query}
 
