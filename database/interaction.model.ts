ed3a354f5054b1191de437f63ec4f5448139c5c5
IanD1013 <ian.tung1013@gmail.com>
2024-11-16 12:46:19 +1300

    Add Mongoose Doc Types for each Model

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -1,18 +1,20 @@
-import { model, models, Schema, Types } from "mongoose";
+import { Schema, models, model, Types, Document } from "mongoose";
 
 export interface IInteraction {
   user: Types.ObjectId;
   action: string;
   actionId: Types.ObjectId;
-  actionType: "question" | "answer";
+  actionType: string;
 }
+
+export interface IInteractionDoc extends IInteraction, Document {}
 
 const InteractionSchema = new Schema<IInteraction>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    action: { type: String, required: true }, // upvote / downvote / view
-    actionId: { type: Schema.Types.ObjectId, required: true }, // question id / answer id / user id
-    actionType: { type: String, enum: ["question", "answer"], required: true }, // question / answer /
+    action: { type: String, required: true }, // 'upvote', 'downvote', 'view', 'ask_question',
+    actionId: { type: Schema.Types.ObjectId, required: true }, // 'questionId', 'answerId',
+    actionType: { type: String, enum: ["question", "answer"], required: true },
   },
   { timestamps: true }
 );
