124d583a17def72513491301d146a8c9b1254381
IanD1013 <ian.tung1013@gmail.com>
2024-12-12 17:30:55 +1300

    Implement api fetch handler

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,9 +5,8 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-// import handleError from "@/lib/handlers/error";
-// import { ValidationError } from "@/lib/http-errors";
-// import dbConnect from "@/lib/mongoose";
+import { api } from "@/lib/api";
+import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
@@ -48,21 +47,22 @@ const questions = [
   },
 ];
 
-// const test = async () => {
-//   try {
-//     throw new ValidationError({ title: ["Required"], tags: ['"Javascript" is not a valid tag'] });
-//   } catch (error) {
-//     return handleError(error);
-//   }
-// };
+const test = async () => {
+  try {
+    return await api.users.getAll();
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  // const result = await test();
-  // console.log(result);
+  const users = await test();
+
+  console.log(users);
 
   const { query = "", filter = "" } = await searchParams;
 
--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
     if (!validatedData.success) throw new ValidationError(validatedData.error.flatten().fieldErrors);
 
     const account = await Account.findOne({ providerAccountId });
-    if (!account) throw new NotFoundError("Accont");
+    if (!account) throw new NotFoundError("Account");
 
     return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
