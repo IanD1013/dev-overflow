b04f353d576b34498e9c9ab17f39eab3aebf27f2
IanD1013 <ian.tung1013@gmail.com>
2024-12-14 10:13:00 +1300

    Implement password/email signin

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import AuthForm from "@/components/forms/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
 import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
-  return (
-    <AuthForm
-      formType="SIGN_IN"
-      schema={SignInSchema}
-      defaultValues={{ email: "", password: "" }}
-      onSubmit={(data) => Promise.resolve({ success: true, data })}
-    />
-  );
+  return <AuthForm formType="SIGN_IN" schema={SignInSchema} defaultValues={{ email: "", password: "" }} onSubmit={signInWithCredentials} />;
 };
 
 export default SignIn;
