ab89d2d0cdddf5a7cdea9916fdec47d3df0fd15b
IanD1013 <ian.tung1013@gmail.com>
2025-05-02 23:58:27 +1200

    Display all answers

--- constants/states.ts
+++ constants/states.ts
@@ -2,7 +2,8 @@ import ROUTES from "./routes";
 
 export const DEFAULT_EMPTY = {
   title: "No Data Found",
-  message: "Looks like the database is taking a nap. Wake it up with some new entries.",
+  message:
+    "Looks like the database is taking a nap. Wake it up with some new entries.",
   button: {
     text: "Add Data",
     href: ROUTES.HOME,
@@ -20,7 +21,8 @@ export const DEFAULT_ERROR = {
 
 export const EMPTY_QUESTION = {
   title: "Ahh, No Questions Yet!",
-  message: "The question board is empty. Maybe it’s waiting for your brilliant question to get things rolling",
+  message:
+    "The question board is empty. Maybe it’s waiting for your brilliant question to get things rolling",
   button: {
     text: "Ask a Question",
     href: ROUTES.ASK_QUESTION,
@@ -36,9 +38,16 @@ export const EMPTY_TAGS = {
   },
 };
 
+export const EMPTY_ANSWERS = {
+  title: "No Answers Found",
+  message:
+    "The answer board is empty. Make it rain with your brilliant answer.",
+};
+
 export const EMPTY_COLLECTIONS = {
   title: "Collections Are Empty",
-  message: "Looks like you haven’t created any collections yet. Start curating something extraordinary today",
+  message:
+    "Looks like you haven’t created any collections yet. Start curating something extraordinary today",
   button: {
     text: "Save to Collection",
     href: ROUTES.COLLECTION,
